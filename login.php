ce7557d3281337ae2974feb91583a3b588a07338
Kamal Nayan <mailtokamalnayan@gmail.com>
2016-12-04 21:01:11 +0530

    Add login

---
+++ login.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Beetle - Login</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+            <form class="col-xs-4">
+                <figure class="text-xs-center">
+                    <img src="http://sparklin.com/beetle/v1.1/img/logo-white.png" class="rounded-circle mt-3 mb-2" width="80" style="background-color: #595959;">
+                </figure>
+                  <div class="form-group">
+                    <label for="formGroupExampleInput">Username/Email</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter username or email">
+                  </div>
+                  <div class="form-group">
+                    <label for="formGroupExampleInput2">Password</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Enter your password">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Log in</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
